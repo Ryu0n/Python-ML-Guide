4905e8856c9e3f16df6dcc63121ea99c63156dec
ryu0n <hsung951027@gmail.com>
2021-03-29 00:08:03 +0900

    Create data_handling_pandas.py.

---
+++ 02. pandas/data_handling_pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+"""
+index : RDBMS의 PK처럼 개별 데이터를 고유하게 식별하는 key값
+Series와 DataFrame 모두 index를 key값으로 가지고 있다.
+Series와 DataFrame의 차이점 : Series는 컬럼이 하나, DataFrame은 컬럼이 여러개 
+즉, DataFrame은 여러개의 Series로 구성되어 있다고 봐도 무방하다.
+"""

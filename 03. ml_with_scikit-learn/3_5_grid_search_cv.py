d407d39b1da12303cadd69c69b89bca9da476db4
Ryu0n <hsung951027@gmail.com>
2021-04-08 13:09:20 +0900

    GridSearchCV

---
+++ 03. ml_with_scikit-learn/3_5_grid_search_cv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+iris = load_iris()
+data = iris.data
+label = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2, random_state=121)
+
+dt_clf = DecisionTreeClassifier()
+
+hyper_params = {'max_depth': [1, 2, 3],
+                'min_samples_split': [2, 3]}
+
+grid_dt_clf = GridSearchCV(estimator=dt_clf, param_grid=hyper_params, scoring='accuracy', cv=3)
+grid_dt_clf.fit(X_train, y_train)
+
+scores_df = pd.DataFrame(grid_dt_clf.cv_results_)
+print(scores_df.columns)
+print(grid_dt_clf.cv_results_)
+print(scores_df)

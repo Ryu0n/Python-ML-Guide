e873ac371b88ce6dee974f411a744236174371ce
ryu0n <hsung951027@gmail.com>
2021-04-16 13:49:12 +0900

    data preprocessing

--- 03. ml_with_scikit-learn/3_7_feature_scailing.py
+++ 03. ml_with_scikit-learn/3_7_feature_scailing.py
@@ -24,6 +24,7 @@ stdev : 표준편차
 
 xi_new = (xi-min(x)) / (max(x)-min(x))
 """
+import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
@@ -37,19 +38,18 @@ iris_feature_names = iris.feature_names
 iris_data = iris.data
 iris_df = pd.DataFrame(data=iris_data, columns=iris_feature_names)
 
-print(iris_df, '\n')
-print(iris_df.mean(), '\n')  # feature 들의 평균값
-print(iris_df.var(), '\n')  # feature 들의 분산값
+# print(iris_df, '\n')
+# print(iris_df.mean(), '\n')  # feature 들의 평균값
+# print(iris_df.var(), '\n')  # feature 들의 분산값
 
 standard_scaler = StandardScaler()
 standard_scaler.fit(iris_df)
 iris_scaled = standard_scaler.transform(iris_df)
 iris_scaled_df = pd.DataFrame(data=iris_scaled, columns=iris_feature_names)
 
-print(iris_scaled, '\n')
-print(iris_scaled_df, '\n')
-print(iris_scaled_df.mean(), '\n')  # 스케일링된 feature 들의 평균값
-print(iris_scaled_df.var(), '\n')  # 스케일링된 feature 들의 분산값
+# print(iris_scaled_df, '\n')
+# print(iris_scaled_df.mean(), '\n')  # 스케일링된 feature 들의 평균값
+# print(iris_scaled_df.var(), '\n')  # 스케일링된 feature 들의 분산값
 
 
 # MinMax Scaler
@@ -58,9 +58,37 @@ min_max_scaler.fit(iris_df)
 iris_normalized = min_max_scaler.transform(iris_df)
 iris_normalized_df = pd.DataFrame(data=iris_normalized, columns=iris_feature_names)
 
-print(iris_normalized, '\n')
 print(iris_normalized_df, '\n')
 print(iris_normalized_df.min(), '\n')  # 정규화된 feature 들의 최소값
 print(iris_normalized_df.max(), '\n')  # 정규화된 feature 들의 최대값
 print(iris_normalized_df.mean(), '\n')  # 정규화된 feature 들의 평균값
 print(iris_normalized_df.var(), '\n')  # 정규화된 feature 들의 분산값
+
+# 학습 데이터 세트 스케일링
+train_array = np.arange(0, 11).reshape(-1, 1)  # 2차원으로 reshape
+test_array = np.arange(0, 6).reshape(-1, 1)
+
+scaler = MinMaxScaler()
+scaler.fit(train_array)
+train_scaled = scaler.transform(train_array)
+"""
+ValueError: Expected 2D array, got 1D array instead:
+array=[ 0.  1.  2.  3.  4.  5.  6.  7.  8.  9. 10.].
+"""
+
+print('원본 train set : ', train_array.reshape(-1))
+print('스케일 train set : ', train_scaled.reshape(-1))
+
+# 잘못된 테스트 데이터 세트 스케일링
+scaler.fit(test_array)
+test_scaled = scaler.transform(test_array)
+
+print('원본 test set : ', test_array.reshape(-1))
+print('스케일 test set : ', test_scaled.reshape(-1))
+
+# 올바른 테스트 데이터 세트 스케일링 (학습 데이터 세트의 fit()으로 테스트 데이터 세트 transform())
+scaler.fit(train_array)
+test_scaled = scaler.transform(test_array)
+
+print('원본 test set : ', test_array.reshape(-1))
+print('스케일 test set : ', test_scaled.reshape(-1))

b39c3c089419e7897d5a2a344a7c343a48b11ebd
Ryu0n <hsung951027@gmail.com>
2021-04-19 18:27:30 +0900

    Titanic

---
+++ 03. ml_with_scikit-learn/3_8_titanic.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+titanic_df = pd.read_csv('../datas/titanic_train.csv')
+print(titanic_df.head(3))
+print(titanic_df.info())
+
+# print(titanic_df['Age'])
+# print(titanic_df['Cabin'])
+# print(titanic_df['Embarked'])
+
+titanic_df['Age'].fillna(titanic_df['Age'].mean(), inplace=True)
+titanic_df['Cabin'].fillna('N', inplace=True)
+titanic_df['Embarked'].fillna('N', inplace=True)
+
+print(titanic_df.isnull())
+print(titanic_df.isnull().sum())
+print(titanic_df.isnull().sum().sum())
+
+print(titanic_df['Sex'].value_counts())
+print(titanic_df['Cabin'].value_counts())
+print(titanic_df['Embarked'].value_counts())

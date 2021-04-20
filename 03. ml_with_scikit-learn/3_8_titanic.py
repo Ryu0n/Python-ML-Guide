236705069f1644319d8c70badfc9317929de6cd5
Ryu0n <hsung951027@gmail.com>
2021-04-20 10:31:12 +0900

    Titanic

--- 03. ml_with_scikit-learn/3_8_titanic.py
+++ 03. ml_with_scikit-learn/3_8_titanic.py
@@ -25,3 +25,13 @@ print(titanic_df['Cabin'].value_counts())
 print(titanic_df['Embarked'].value_counts())
 
 titanic_df['Cabin'] = titanic_df['Cabin'].str[:1]  # 문자열의 앞에 한글자만 추출
+print(titanic_df['Cabin'].head(3))
+print(titanic_df.groupby(['Sex', 'Survived'])['Survived'].count())
+
+# 성별에 따른 생존률
+sns.barplot(x='Sex', y='Survived', data=titanic_df)
+plt.show()
+
+# Pclass : 객실
+sns.barplot(x='Pclass', y='Survived', hue='Sex', data=titanic_df)
+plt.show()
